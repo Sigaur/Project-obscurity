57019d8a48fd3edfc519e5e23518cd0d986ae65f
auda <brionmathieu@gmail.com>
2015-08-22 16:45:30 +0200

    creation de la map et modification ponctuel

--- include/Box.h
+++ include/Box.h
@@ -1,8 +1,51 @@
 #pragma once
+
+
+#include <iostream>
+
+
+#define NBCASE 5
+#define NBLIGNE 10
+#define NBTYPE 5
+using namespace std;
+
+
+
+
+
 class Box
 {
+private:
+	int _contenu;//0= vide   1=un obstacle physique  2=personnage
+
 public:
+	/************************            Constructeur Destructeur          *****************************/
+
 	Box();
-	~Box();
+	Box(int contenu);
+	Box(const Box&);
+	~Box(){};
+
+
+	/************************            get set                           *****************************/
+
+	int   getContenu()const;
+	void  setContenu(int contenu);
+
+	/************************            fonction                          *****************************/
+	
+
+
+	/************************            operator                          *****************************/
+	Box operator = (const Box& original);
+
+
+
+	friend class Level;
+
 };
+
+void afficher_box(Box Box);
+Box* creationType();
+
 
--- include/Level.h
+++ include/Level.h
@@ -1,8 +1,41 @@
 #pragma once
+#include "Box.h"
+
+
+
+
 class Level
 {
+private:
+	Box ** _carte;
+
 public:
+
+	/************************            Constructeur Destructeur          *****************************/
 	Level();
+	Level(const Level&);
 	~Level();
+
+
+	/************************            get set                           *****************************/
+
+
+
+	/************************            fonction                          *****************************/
+	void setBox(int y, int x, Box valeur);
+	Box getBox(int y, int x);
+
+	void setMap(Box**);
+	Box**  getMap();
+
+
+
+	friend class Box;
+
+
+
 };
 
+
+
+void affichage_Level(Level* leniveau);
